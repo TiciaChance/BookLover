19a4a85303fe8d06b01a042f53de7f7aedfa205b
TiciaChance <laticia.chance@gmail.com>
2016-12-10 12:22:30 -0500

    started ui, added pagevc

---
+++ BookLover/BookLoverPageVC.swift
@@ -0,0 +1,80 @@
+//
+//  BookLoverPageVC.swift
+//  BookLover
+//
+//  Created by Laticia Chance on 12/10/16.
+//  Copyright © 2016 Laticia Chance. All rights reserved.
+//
+
+import UIKit
+
+class BookLoverPageVC: UIPageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+        
+    }
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newPageVC(VCorder: "first"),
+                self.newPageVC(VCorder: "second"),
+                self.newPageVC(VCorder: "third")]
+    }()
+    
+    private func newPageVC(VCorder: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(VCorder)VC")
+    }
+}
+
+extension BookLoverPageVC: UIPageViewControllerDataSource {
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+}
